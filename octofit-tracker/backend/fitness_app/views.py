abf2e8d899951313b078763680fd6b756527761f
jsferrer1 <jsferrer1@yahoo.com>
2025-04-08 21:56:07 +0000

    Document OctoFit Tracker story and workshop details

---
+++ octofit-tracker/backend/fitness_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Team, Activity, Leaderboard, Workout
+from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': 'http://localhost:8000/api/users/',
+        'teams': 'http://localhost:8000/api/teams/',
+        'activities': 'http://localhost:8000/api/activities/',
+        'leaderboard': 'http://localhost:8000/api/leaderboard/',
+        'workouts': 'http://localhost:8000/api/workouts/'
+    })
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+class LeaderboardViewSet(viewsets.ModelViewSet):
+    queryset = Leaderboard.objects.all()
+    serializer_class = LeaderboardSerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
